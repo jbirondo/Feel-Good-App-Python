ee6c2f9b1946724fbbabf1a1b385731cae403923
Eugene Birondo <jbirondo@gmail.com>
2020-09-02 00:33:51 -0700

    Setting up LoginScreen

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 Builder.load_file("design.kv")
+
+class LoginScreen(Screen):
+    pass
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
